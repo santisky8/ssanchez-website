d2789bc21495f8be6512f458c7d89f14a84def8b
santisky8 <113253355+santisky8@users.noreply.github.com>
2022-10-20 16:58:54 +0100

    oct20-2

--- Modules/main.js
+++
@@ -1,5 +0,0 @@
-//import the functions halfOf from the lib.js module
-
-import { halfOf, multiply } from './lib.js';
-console.log("halfOf: " + halfOf(84) );
-console.log("multiply:" + multiply(84, 2) ); 
