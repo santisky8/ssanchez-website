8f7d9952712571362aa01f8d54928cb950194595
Santiago Sanchez <s.sanchezcalle@gmail.com>
2023-05-12 19:54:55 -0400

    may 12
    Correction on port 3000

--- routes/index.js
+++ routes/index.js
@@ -78,11 +78,3 @@ router.get('/contact', function(req, res, next) {
 });
 
 module.exports = router;
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
